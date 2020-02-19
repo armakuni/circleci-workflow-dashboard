7956ee98d518dbca652489aaa8a62cc1a626e168
Lewis England <lewis.england@armakuni.com>
2020-02-19 17:26:25 +0000

    Tidy and comment the circleci.py code. Read the circleCI token from env var. Fix the CSS classes to match the output from the API

--- circleci.py
+++ circleci.py
@@ -1,3 +1,4 @@
+import os
 import requests
 
 STATUS_SUCCESS = "success"
@@ -19,11 +20,13 @@ COMPLETED_STATUSES = [
     STATUS_UNKNOWN,
 ]
 
-api_token = ""
+api_token = os.environ['CIRCLECI_TOKEN']
 api_response = {}
 
 
-# Generic calls to each of the APIs
+# Generic calls to each of the APIs (v1.1, v2)
+# Input: string
+# Output: Array{}
 def call_api_v1(api_target):
     response = requests.get(
         f"https://circleci.com/api/v1.1/{api_target}", auth=(api_token, "")
@@ -31,6 +34,10 @@ def call_api_v1(api_target):
     return response.json()
 
 
+# v2 seems to have introduced pagination.
+# Take the stress of following the tokens out of the business logic
+# Input: string, boolean
+# Output: Array{}
 def call_api_v2(api_target, handle_pagination):
     api_url = f"https://circleci.com/api/v2/{api_target}"
     if not handle_pagination:
@@ -49,15 +56,23 @@ def call_api_v2(api_target, handle_pagination):
         return all_results
 
 
-# For each project get all pipelines (using project slug)
+# Get all the projects for the authenticated user (API_TOKEN)
+# Input: None
+# Output: Array{}
 def get_all_projects():
     return call_api_v1("projects")
 
 
+# Get all the Pipelines under a given project_slug
+# Input: string
+# Output: Array{}
 def get_all_pipelines(project_slug):
     return call_api_v2(f"project/{project_slug}/pipeline", True)
 
 
+# Get a dictionary of branches with the associated Pipeline objects
+# Input: string
+# Output: Array{}
 def filter_pipeline_per_branch(pipelines):
     filtered_pipelines = {}
     for pipeline in pipelines:
@@ -68,6 +83,9 @@ def filter_pipeline_per_branch(pipelines):
     return filtered_pipelines
 
 
+# Get a dictionary of branches with the associated Pipeline IDs
+# Input: string
+# Output: Array{}
 def get_latest_pipeline_per_branch(pipelines):
     latest_pipeline_ids = {}
     for pipeline in pipelines:
@@ -78,59 +96,78 @@ def get_latest_pipeline_per_branch(pipelines):
     return latest_pipeline_ids
 
 
+# Get all the Workflows used by a Pipeline
+# Input: string
+# Output: Array{}
 def get_workflows_for_pipeline(pipeline_id):
     return call_api_v2(f"pipeline/{pipeline_id}/workflow", True)
 
 
+# Get all the Jobs run by a Workflow
+# Input: string
+# Output: Array{}
 def get_jobs_for_workflow(workflow_id):
     return call_api_v2(f"workflow/{workflow_id}/job", True)
 
 
+# Generate a URI to the targeted Pipeline run
+# Input: string, string, string
+# Output: string
 def workflow_link(project_slug, pipeline_id, workflow_id):
-
-    #   https://app.circleci.com/github/srbry/hello-world-circle/pipelines/666942a9-41d2-4239-8fc3-babd6e0e4cd0/workflows/fa0d75e8-d594-4e37-832f-28a4627da3bb
-    #   https://app.circleci.com/jobs/github/srbry/hello-world-circle/98
     return f"https://app.circleci.com/{project_slug}/pipelines/{pipeline_id}/workflows/{workflow_id}"
 
 
+# Generate a URI to the targeted Job
+# Input: string, string
+# Output: string
+def job_link(project_slug, job_id):
+    return f"https://app.circleci.com/jobs/{project_slug}/{job_id}"
+
+
+# Iterate through previous Pipeline runs to find the last completed run for a Workflow
+# Input: string, string
+# Output: string
 def get_previous_completed_status(filtered_pipelines, workflow_name):
     status = STATUS_UNKNOWN
     for pipeline in filtered_pipelines:
-        worklow_in_pipeline = False
+        workflow_in_pipeline = False
         for workflow in get_workflows_for_pipeline(pipeline["id"]):
             if workflow["name"] == workflow_name:
-                worklow_in_pipeline = True
+                workflow_in_pipeline = True
                 if workflow["status"] in COMPLETED_STATUSES:
                     return workflow["status"]
-        if not worklow_in_pipeline:
+        if not workflow_in_pipeline:
             return status
     return status
 
 
 def get_dashboard_data():
-    # {
-    #       "name": "Project X",
-    #       "workflow": "Workflow Y",
-    #       "branch": "Branch Z",
-    #       "status": "success running",
-    #   }
     compound_keys = []
     dashboard_data = []
+    # Iterate all projects followed in CircleCI
     for project in get_all_projects():
+        # Generate the 'project_slug' for each one in the format :vcs_type/:vcs_username/:vcs_project_name
         project_slug = (
             f"{project['vcs_type']}/{project['username']}/{project['reponame']}"
         )
+        # Get the pipelines (runs) associated with the project
         pipelines = get_all_pipelines(project_slug)
+        # Filter the pipelines down into the VCS branches they ran for
         filtered_pipelines = filter_pipeline_per_branch(pipelines)
+        # Iterate over the branches in the project, focussing on the last pipeline run of each
         for branch, pipeline_id in get_latest_pipeline_per_branch(pipelines).items():
-            # print(f"Branch: {branch}")
+            # Iterate over a list of the workflows used by the lastest pipeline run (normally only one)
             for workflow in get_workflows_for_pipeline(pipeline_id):
+                # Generate a compound key to identify the pipeline uniquely
                 compound_key = f"{project['reponame']}{workflow['name']}-{branch}"
+                # Check to see if that key already exists, if it does - ignore this workflow / pipeline run
                 if compound_key in compound_keys:
                     continue
+                # For "running state" (states with animated borders), we need to append the correct secondary state
                 status = workflow["status"]
                 if workflow["status"] not in COMPLETED_STATUSES:
                     status += f" {get_previous_completed_status(filtered_pipelines[branch], workflow['name'])}"
+                # Finally, put all this working out together, append that to the returned data, and start all over again
                 dashboard_data.append(
                     {
                         "name": project["reponame"],
@@ -142,15 +179,6 @@ def get_dashboard_data():
                         ),
                     }
                 )
-                # print(
-                #     f"  Name: {workflow['name']}, Status: {workflow['status']}, ID: {workflow['id']}"
-                # )
+                # Append the compound key to show we've processed this project/branch/workflow
                 compound_keys.append(compound_key)
-
-            # for job in get_jobs_for_workflow(workflow["id"]):
-            #     print(f"    Job: {job['name']}, Status: {job['status']}")
     return dashboard_data
-
-
-# Get latest completed build
-# Previous versions with matching workflow name within same pipeline
