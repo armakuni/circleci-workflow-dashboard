073717b3ee9970fe8c351957a2d7847063131d3c
Lewis England <lewis.england@armakuni.com>
2020-02-19 16:37:44 +0000

    Importing the renamed circleci package

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-import foo
+import circleci
 
 app = Flask(__name__, static_folder="assets", static_url_path="")
 
@@ -9,7 +9,7 @@ app = Flask(__name__, static_folder="assets", static_url_path="")
 def hello_world():
     return render_template(
         "dashboard.html",
-        projects=foo.get_dashboard_data(),
+        projects=circleci.get_dashboard_data(),
         refreshInterval=30,
         now="The time now",
     )
