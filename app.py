ea97e56710fc21b327da7cdebb0be2f3d472f426
Sam Bryant <srbry@hotmail.com>
2020-02-20 08:59:19 +0000

    Set the time the dashboard view is for in UTC

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from datetime import datetime, timezone
 import os
 import circleci
 
@@ -8,11 +9,16 @@ api_token = os.environ["CIRCLECI_TOKEN"]
 circleci_client = circleci.CircleCI(api_token)
 
 
+def current_time():
+    now = datetime.now(timezone.utc)
+    return now.strftime("%y-%m-%d %H:%M:%S %Z")
+
+
 @app.route("/")
 def hello_world():
     return render_template(
         "dashboard.html",
         projects=circleci.get_dashboard_data(circleci_client),
         refreshInterval=30,
-        now="The time now",
+        now=current_time(),
     )
