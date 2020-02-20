f230005dab337bf43d974897b7065a07ce3122c7
Sam Bryant <srbry@hotmail.com>
2020-02-20 16:14:56 +0000

    Swap to usising twisted web framework for better threading

--- app.py
+++ app.py
@@ -1,47 +1,16 @@
-from flask import Flask, render_template
+from twisted.web import server, resource, static
+from twisted.internet import reactor, endpoints, task
 from datetime import datetime, timezone
-import threading
-import atexit
+import jinja2
 import os
 import circleci
 
+
 REFRESH_INTERVAL = 30
-projects = {}
 
-# lock to control access to variable
-circleLock = threading.Lock()
-# thread handler
-fetchCircleDataThread = threading.Thread()
-
-
-def create_app(circleci_client):
-    app = Flask(__name__, static_folder="assets", static_url_path="")
-
-    def interrupt():
-        global fetchCircleDataThread
-        fetchCircleDataThread.cancel()
-
-    def update_projects():
-        global projects
-        global fetchCircleDataThread
-        with circleLock:
-            projects = circleci.get_dashboard_data(circleci_client)
-
-        fetchCircleDataThread = threading.Timer(REFRESH_INTERVAL, update_projects, ())
-        fetchCircleDataThread.start()
-
-    def init_projects():
-        global fetchCircleDataThread
-        global projects
-        projects = circleci.get_dashboard_data(circleci_client)
-        fetchCircleDataThread = threading.Timer(REFRESH_INTERVAL, update_projects, ())
-        fetchCircleDataThread.start()
-
-    # Initiate
-    init_projects()
-    # When you kill Flask (SIGTERM), clear the trigger for the next thread
-    atexit.register(interrupt)
-    return app
+port = os.getenv("PORT", "5000")
+api_token = os.environ["CIRCLECI_TOKEN"]
+circleci_client = circleci.CircleCI(api_token)
 
 
 def current_time():
@@ -49,17 +18,36 @@ def current_time():
     return now.strftime("%y-%m-%d %H:%M:%S %Z")
 
 
-api_token = os.environ["CIRCLECI_TOKEN"]
-circleci_client = circleci.CircleCI(api_token)
-app = create_app(circleci_client)
+class Dashboard(resource.Resource):
+    projects = {}
+    current_time = ""
+
+    def update_projects(self):
+        self.projects = circleci.get_dashboard_data(circleci_client)
+        self.current_time = current_time()
+
+    def render_GET(self, request):
+        templateLoader = jinja2.FileSystemLoader(searchpath="./templates")
+        templateEnv = jinja2.Environment(loader=templateLoader)
+        template = templateEnv.get_template("dashboard.html")
+
+        return template.render(
+            projects=self.projects,
+            refreshInterval=REFRESH_INTERVAL,
+            now=self.current_time,
+        ).encode("utf-8")
 
 
-@app.route("/")
-def hello_world():
-    global projects
-    return render_template(
-        "dashboard.html",
-        projects=projects,
-        refreshInterval=REFRESH_INTERVAL,
-        now=current_time(),
-    )
+dashboard = Dashboard()
+
+root = resource.Resource()
+root.putChild(b"assets", static.File("./assets"))
+root.putChild(b"", dashboard)
+site = server.Site(root)
+
+project_updater = task.LoopingCall(dashboard.update_projects)
+project_updater.start(REFRESH_INTERVAL)
+
+print(f"Starting server on http://127.0.0.1:{port}")
+reactor.listenTCP(int(port), site)
+reactor.run()
