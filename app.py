1f1794a9d1333ea0d7fc90232b8e057b42651883
Sam Bryant <srbry@hotmail.com>
2020-03-04 12:25:32 +0000

    Add ENV var for refresh interval

--- app.py
+++ app.py
@@ -7,12 +7,11 @@ import circleci
 import json
 
 
-REFRESH_INTERVAL = 30
-
 port = os.getenv("PORT", "5000")
 circleci_api_url = os.getenv("CIRCLECI_API_URL", circleci.DEFAULT_API_URL)
 circleci_jobs_url = os.getenv("CIRCLECI_JOBS_URL", circleci.DEFAULT_JOBS_URL)
 circleci_dashboard_filter = json.loads(os.getenv("DASHBOARD_FILTER", "null"))
+refresh_interval = int(os.getenv("REFERSH_INTERVAL", "30"))
 
 api_token = os.getenv("CIRCLECI_TOKEN", None)
 if api_token is None:
@@ -44,7 +43,7 @@ class Dashboard(resource.Resource):
 
         return template.render(
             projects=self.projects,
-            refreshInterval=REFRESH_INTERVAL,
+            refreshInterval=refresh_interval,
             now=self.current_time,
         ).encode("utf-8")
 
@@ -57,7 +56,7 @@ root.putChild(b"", dashboard)
 site = server.Site(root)
 
 project_updater = task.LoopingCall(dashboard.update_projects)
-project_updater.start(REFRESH_INTERVAL)
+project_updater.start(refresh_interval)
 
 print(f"Starting server on http://127.0.0.1:{port}")
 reactor.listenTCP(int(port), site)
